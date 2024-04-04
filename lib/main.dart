016511dd17189388a9d7f803c0b123de21ecb495
Admin <bthai865@gmail.com>
2024-04-04 14:05:18 +0700

    feat: notifications config

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:ssps_app/firebase_options.dart';
 import 'package:ssps_app/pages/homePage.dart';
+import 'package:ssps_app/pages/todolistPage.dart';
 import 'package:ssps_app/pages/welcomPage.dart';
+import 'package:ssps_app/push_notification.dart';
 import 'package:ssps_app/service/shared_service.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 
 Widget _defaultHome = const WelcomePage();
+
+final naviatorKey = GlobalKey<NavigatorState>();
+
+Future _firebaseBackgroundMessage(RemoteMessage message) async {
+  if (message.notification != null) {
+    print("Some notification  received");
+  }
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,8 +26,32 @@ void main() async {
     statusBarColor: Colors.transparent,
   ));
 
-  bool _result  = await SharedService.isLoggedIn();
-  if(_result) {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+    if (message.data['page'] != null) {
+      String page = message.data['page'];
+      print("background notification tapped, page: $page");
+
+      if (page == 'home') {
+        naviatorKey.currentState
+            ?.push(MaterialPageRoute(builder: (context) => HomePage()));
+      } else if (page == 'todo') {
+        naviatorKey.currentState
+            ?.push(MaterialPageRoute(builder: (context) => TodolistPage()));
+      }
+    }
+  });
+
+  FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+
+  PushNotifications.init();
+  FirebaseMessaging.onBackgroundMessage(_firebaseBackgroundMessage);
+
+  bool _result = await SharedService.isLoggedIn();
+  if (_result) {
     print(_result);
     _defaultHome = HomePage();
   }
@@ -27,12 +65,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: naviatorKey,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: ('inter'),
         useMaterial3: true,
       ),
       home: _defaultHome,
+      routes: {
+        // '/': (context) => HomePage(),
+      },
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,18 @@ import Foundation
 
 import audioplayers_darwin
 import file_selector_macos
+import firebase_analytics
+import firebase_core
+import firebase_messaging
 import path_provider_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
